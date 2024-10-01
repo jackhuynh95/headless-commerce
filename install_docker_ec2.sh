e4f159ae454aa97d18faa1074b972488bf214297
Jack Huynh <kietdien88@gmail.com>
2024-10-01 14:16:04 +0700

    Update install_docker_ec2.sh
    Manage Docker as a Non-root User (Optional)

--- install_docker_ec2.sh
+++ install_docker_ec2.sh
@@ -5,3 +5,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce
+sudo usermod -aG docker ubuntu
