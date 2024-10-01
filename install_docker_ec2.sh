84d941d50ebf2f8a3c382c8bf2f0f9ddde07d396
Jack Huynh <kietdien88@gmail.com>
2024-10-01 13:59:12 +0700

    Update install_docker_ec2.sh
    Convert EC2 Ubuntu instance

--- install_docker_ec2.sh
+++ install_docker_ec2.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
-yum update -y
-amazon-linux-extras install docker
-service docker start
-usermod -a -G docker ec2-user
-chkconfig docker on
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
